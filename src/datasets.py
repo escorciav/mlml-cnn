e9c3e5c2615d8929728cc2b3e3f36ec4da631a1b
Victor Escorcia <escorciav@gmail.com>
2015-05-26 12:19:39 +0300

    Add function to dump annotations of ESP-Game dataset as hdf5 file. Input source is loaded from pre-computed data provided by Bao

--- src/datasets.py
+++ src/datasets.py
@@ -1,6 +1,10 @@
 import glob
 import os
 
+from oct2py import octave
+
+import utils
+
 def espgame_dump_list(filename, outfile, ext='.jpg', root=None):
     fid_o = open(outfile, 'w')
     with open(filename, 'r') as fid_i:
@@ -13,4 +17,12 @@ def espgame_dump_list(filename, outfile, ext='.jpg', root=None):
                 line += ext
             fid_o.write(line + '\n')
     fid_o.close()
+
+def espgame_dump_labels(filename, outfile, attrb):
+    # Add ESP-game dataset to octave path
+    octave.addpath('data/ESP-Game/')
+    # Use vec_read octave-function to load data
+    info = octave.vec_read(filename)
+    # Dump data into a standard format
+    utils.h5py_save(outfile, **{attrb:info.T})
 
